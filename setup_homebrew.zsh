3d3e998f1eb9f8447ad0927c7b70d2f3220bff47
markabrennan <markangusbrennan@gmail.com>
2024-03-06 15:08:52 -0500

    changes as advised by ChatGPT.

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -3,14 +3,19 @@
 #  "Borrowed" from 
 #  https://github.com/eieioxyz/dotfiles_macos/blob/master/setup_homebrew.zsh
 #
+
+# Update sudo timestamp
+sudo -v
+
 echo "\n<<< Starting Homebrew Setup >>>\n"
 
-if exists brew; then
+if type brew >/dev/null 2>&1; then
   echo "brew exists, skipping install"
 else
   echo "brew doesn't exist, continuing with install"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+
 
 # Depends on Brewfile here:
 brew bundle --verbose
