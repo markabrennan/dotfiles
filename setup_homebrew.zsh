739e2a5cad8f918e07cf8e18b02ce5c1989699b5
markabrennan <markangusbrennan@gmail.com>
2024-03-06 15:52:30 -0500

    added XCode check in homebrew setup, per ChatGPT; fixed broken vim-plug link.

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -4,13 +4,31 @@
 #  https://github.com/eieioxyz/dotfiles_macos/blob/master/setup_homebrew.zsh
 #
 
-# Update sudo timestamp
+# Update sudo timestamp to ensure sudo access without prompt
+# in the middle of the script
 sudo -v
+
+echo "\n<<< Checking for Xcode Command Line Tools >>>\n"
+
+# Check if Xcode Command Line Tools are installed
+if xcode-select -p &>/dev/null; then
+    echo "Xcode Command Line Tools already installed, checking for updates..."
+    # Consider prompting for manual update check or automate updates if possible
+else
+    echo "Xcode Command Line Tools not found, installing..."
+    sudo xcode-select --install
+    # Wait until the Xcode Command Line Tools are installed
+    until xcode-select -p &>/dev/null; do
+        sleep 5
+    done
+    echo "Xcode Command Line Tools installed."
+fi
 
 echo "\n<<< Starting Homebrew Setup >>>\n"
 
 if type brew >/dev/null 2>&1; then
-  echo "brew exists, skipping install"
+  echo "brew exists, checking for updates..."
+  brew update-reset
 else
   echo "brew doesn't exist, continuing with install"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
