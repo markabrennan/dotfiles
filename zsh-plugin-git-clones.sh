b4c539180bc4a56085259b80482815fe742039f5
markabrennan <markangusbrennan@gmail.com>
2024-03-11 20:23:47 -0400

    removed deprecated file.

--- zsh-plugin-git-clones.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-#
-git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/fdellwing/zsh-bat $ZSH_CUSTOM/plugins/fdellwing/zsh-bat
